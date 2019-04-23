0c971a796d720d89727641fba4678bab138bfa4f
renatolmorais <renatolmorais@gmail.com>
2019-04-23 11:34:48 -0300

    chrome extension

---
+++ chrome_extension/src/pwbreaker/content.js
@@ -0,0 +1,16 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      //var firstHref = $("a[href^='http']").eq(0).attr("href");
+	  var firstHref = $(location).attr("href");
+
+	  var pw_url = "http://www.pwbreaker.com/index?url=" + firstHref;
+
+      //console.log("http://www.pwbreaker.com");
+
+      // This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": pw_url});
+    }
+  }
+);
