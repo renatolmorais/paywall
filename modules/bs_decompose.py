ed99404e455e252cdd02e35b36e07c4b69b656d9
renatolmorais <renatolmorais@gmail.com>
2019-10-31 14:37:23 -0300

    arquivos para testar paywall breaking

---
+++ modules/bs_decompose.py
@@ -0,0 +1,57 @@
+#!encoding: UTF-8
+
+from requests import Session
+import BeautifulSoup as bs
+from re import match
+from tags import tags_to_remove, host_tags_to_remove
+
+'''js-touchpoint c-touchpoint-post-footer
+js-touchpoint c-touchpoint-post-footerFixed
+c-communication-errors user-report
+breadcrumb
+
+host1
+	tag1 attr1 attr2 ...
+	tag2 attr1 attr2 ...
+host2
+	tag1 attr1 attr2 ...
+	tag2 attr1 attr2 ...
+...'''
+
+def decompose(host,html_text):
+	page = bs.BeautifulSoup(html_text.encode('utf-8'))
+	'''
+	...
+	'''
+	# remover tags
+	for tag in tags_to_remove:
+		while page.find(tag): page.find(tag).decompose()
+	# remover tags por host
+	host_tags_to_remove = tags_to_remove_by_host.get(host,{})
+	for tag,attrlist in host_tags_to_remove.iteritems():
+		if tag == 'remove':
+			for tagname in attrlist:
+				while page.find(tagname): page.find(tagname).decompose()
+		else:
+			for attr,list_of_values in attrlist.iteritems():
+				for value in list_of_values:
+					while page.find(tag,attrs={attr:value}):
+						page.find(tag,attrs={attr:value}).decompose()
+	return str(page)
+	
+if __name__ == '__main__':
+
+	proxies = {'http':'http://proxy.almg.uucp:3128/','https':'http://proxy.almg.uucp:3128/'}
+	#url = 'https://www.em.com.br/app/colunistas/luiz-carlos-azedo/2019/10/30/interna_luiz_carlos_azedo,1096859/a-tuitada-infeliz-dos-bolsonaro-sobre-leao-e-hienas.shtml'
+	url = 'https://www.gazetadopovo.com.br/opiniao/bom-dia/a-morte-de-marielle-franco-e-o-que-o-principe-da-arabia-saudita-quer-com-o-brasil/'
+	
+	url_regex = 'htt(p|ps)://(?P<host>.*?)/.*'
+	host = match(url_regex,url).group('host')
+	
+	ses = Session()
+	resp = ses.get(url,proxies=proxies)
+	with open('antes.html','w') as fp: fp.write(resp.text.encode('utf-8'))
+	dec = decompose(host,resp.text)
+	with open('depois.html','w') as fp: fp.write(dec)
+	#print decompose(host,resp.text)
+	
---
+++ modules/tags.py
@@ -0,0 +1,30 @@
+tags_to_remove_by_host = {
+	'www.em.com.br':
+	{
+		'remove':	['aside'],
+		'div':		{'class':	['login-signature-call-bottom hidden-no-important show-loginwall-login-active-inline','nav-side__container','news-card__content','news-blocked-content','news-blocked js-news-blocked','news-blocked js-news-blocked-login'] },
+		'small':	{'class':	['hidden-print txt-no-serif',] },
+		'p':		{'class':	['text-xs-center',] },
+		'footer':	{'class':	['footer bg-gray-extra',] },
+		'nav':		{'class':	['fixed-btn-uai','nav-main','nav-main pt-8 pb-8 pt-xs-7 pb-xs-7'] },
+		'section':	{'class':	['bg-gray-extra mt-20','bg-gray-extra'] },
+	},
+	'super.abril.com.br':
+	{
+		'header':	{'class':	['header'] },
+		'div':		{'class':	['sidebar-top hidden-xs hidden-sm','widget-cover-container'] },
+		'section':	{'class':	['comments','sidebar-above-footer',] },
+	},
+	'www.gazetadopovo.com.br':
+	{
+		'remove':	['aside'],
+		'div':		{'class':	['header','wrapper','breadcrumb','js-touchpoint c-touchpoint-post-footer','js-touchpoint c-touchpoint-post-footerFixed','c-communication-errors user-report',] },
+		'footer':	{'class':	['c-footer'],},
+	},
+	'gazetaonline.globo.com':
+	{
+		'remove':	['iframe'],
+	}
+}
+
+tags_to_remove = ['script','iframe','noscript','header']
